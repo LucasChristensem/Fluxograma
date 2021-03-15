0ef0cfde23308a3dd70e677b95c5de83b454b0be
LucasChristensem <62951701+LucasChristensem@users.noreply.github.com>
2021-03-15 01:15:32 -0300

    Update database.js

--- js/database.js
+++ js/database.js
@@ -3,7 +3,7 @@ window.semesters = 10;
 
 /* List of available colors to toggle a course as completed */
 /* The first color is default for selecting options as well, so make sure to choose that one wisely */
-window.colors = ["#b1fca4", "#fac6ff", "#a8c9ff"];
+window.colors = ["#b1fca4", "#fac6ff", "#a8c9ff", "#ad3d39", "#a104d1", "#11ede6"];
 
 // Order of courses matters: do not swap positions. Append at the end.
 window.courses = [
